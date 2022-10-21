cc09294c5bd330e331605ca87556afa57919a5c6
Chirag-01 <lalwanicp@rknec.edu>
2022-10-21 22:57:38 +0530

    I have added my code for Stack in Python and its working on leetcode

--- Implment Stack in Python.py
+++ Implment Stack in Python.py
@@ -1,9 +1,23 @@
-a=list(map(int,input().split()))
+# For leetcode
+# My submission https://leetcode.com/submissions/detail/827433145/
+# Please copy paste below code to check for leet code
+# class Solution:
+#     def buildArray(self, target: List[int], n: int) -> List[str]:
+#           res = []
+#           s = set(target)
+#           for i in range(1, target[-1] + 1):
+#                 res.append("Push")
+#                 if i not in s:
+#                     res.append("Pop")
+#           return res
+
+
 n=int(input())
+target=list(map(int,input().split()))
 res = []
-s = set(a)
-for i in range(1, a[-1] + 1):
-        res.append("Push")
-        if i not in s:
-            res.append("Pop")
+s = set(target)
+for i in range(1, target[-1] + 1):
+         res.append("Push")
+         if i not in s:
+                res.append("Pop")
 print(res)
