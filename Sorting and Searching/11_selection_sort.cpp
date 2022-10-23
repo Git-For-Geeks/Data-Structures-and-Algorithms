e961374ad34238256653d3c6dbf74836d44a1245
devashree99 <ambegaonkard@rknec.edu>
2022-10-23 20:52:08 +0530

    selection sort added

---
+++ Sorting and Searching/11_selection_sort.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+     void selectionSort(int arr[], int n)
+    {
+      
+       for(int i=0;i<n;i++){
+           int mini=i;
+           for(int j=i+1;j<n;j++){
+               if(arr[j]<arr[mini]) mini=j;
+           }
+           swap(arr[i],arr[mini]);
+       }
+
+    return 0;
+}
