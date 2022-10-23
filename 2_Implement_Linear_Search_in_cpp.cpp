d5f3e6089c6e8d140a3c6ab50eae3324821f0192
Mrugal <dudhbademr@rknec.edu>
2022-10-23 18:40:06 +0530

    Added Linear search in .cpp

---
+++ 2_Implement_Linear_Search_in_cpp.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to search x in arr
+// arr: input array
+// X: element to be searched for
+int search(int arr[], int N, int X)
+{
+
+    for (int i = 0; i < N; i++)
+    {
+        if (arr[i] == X)
+        {
+            return i;
+            break;
+        }
+    }
+    return -1;
+}
+
+// { Driver Code Starts.
+
+int main()
+{
+    int testcases;
+    cin >> testcases;
+    while (testcases--)
+    {
+        int sizeOfArray;
+        cin >> sizeOfArray;
+        int arr[sizeOfArray];
+        int x;
+
+        for (int i = 0; i < sizeOfArray; i++)
+        {
+            cin >> arr[i];
+        }
+        cin >> x;
+        cout << search(arr, sizeOfArray, x) << endl; // Linear search
+    }
+
+    return 0;
+}
+// } Driver Code Ends
