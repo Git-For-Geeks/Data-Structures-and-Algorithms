f605dbae51004d3e51e80420aa2c283545a7d7a0
Divyanshu Wakodikar <divyanshuwakodikar256@gmail.com>
2022-10-27 01:56:01 +0530

    Longest substring without repeating char

---
+++ Strings/Longest_Substring_Without_Repeating_Characters.py
@@ -0,0 +1,27 @@
+
+def longestUniqueSubsttr(str):
+	
+	n = len(str)
+
+	res = 0
+
+	for i in range(n):
+		visited = [0] * 256
+
+		for j in range(i, n):
+			if (visited[ord(str[j])] == True):
+				break
+			else:
+				res = max(res, j - i + 1)
+				visited[ord(str[j])] = True
+		visited[ord(str[i])] = False
+	
+	return res
+
+str = "hacktoberfestfest"
+print("The input is ", str)
+
+len = longestUniqueSubsttr(str)
+print("The length of the longest "
+	"non-repeating character substring is ", len)
+
