aef97fc9c4fc58aa6651711f3168b0247c391865
darshanChandan <chandandb@rknec.edu>
2022-10-29 10:31:38 +0530

    Solution added to Implement Binary Search in JS

--- sort_and_search/5_binary_search.js
+++ sort_and_search/5_binary_search.js
@@ -1,21 +1,19 @@
 class Solution {
-    binarysearch(arr, n, k) {
-        let begin = 0;
-        let last = n - 1;
-        
-        while( begin <= last ){
-            let middle = Math.floor((begin + last) / 2);
-            
-            if(arr[middle] == k){
-                return middle;  //key is found
-            }
-            else if(arr[middle] < k){
-                begin = middle + 1;
-            }
-            else {
-                last = middle - 1;
-            }
-        }
-        return -1;
+  binarysearch(arr, n, k) {
+    let begin = 0;
+    let last = n - 1;
+
+    while (begin <= last) {
+      let middle = Math.floor((begin + last) / 2);
+
+      if (arr[middle] == k) {
+        return middle; //key is found
+      } else if (arr[middle] < k) {
+        begin = middle + 1;
+      } else {
+        last = middle - 1;
+      }
     }
+    return -1; //key is not found
+  }
 }
